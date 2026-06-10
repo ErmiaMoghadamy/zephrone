b9d3c41a95895a1e37dae7d49b197d668a684d9f
Ermia <ermia.codev@gmail.com>
2026-06-10 19:40:10 +0330

    textured shader & texture smoke test

--- src/app.zig
+++ src/app.zig
@@ -12,6 +12,10 @@ pub const App = struct {
     window: *Window,
     allocator: std.mem.Allocator,
     io: std.Io,
+    mesh1: BlockMesh,
+    shader1: Shader,
+    camera1: Camera,
+    texture1: Texture,
 
     pub fn init(io: std.Io, allocator: std.mem.Allocator) !*App {
         const app = try allocator.create(App);
@@ -27,6 +31,13 @@ pub const App = struct {
             .window = window,
             .allocator = allocator,
             .io = io,
+            .camera1 = Camera.init(@as(f32, @floatFromInt(window.data.width)) / @as(f32, @floatFromInt(window.data.height))),
+            .mesh1 = BlockMesh.init(),
+            .shader1 = try Shader.init(
+                @embedFile("shaders/textured.vert.glsl"),
+                @embedFile("shaders/textured.frag.glsl"),
+            ),
+            .texture1 = try Texture.init(io, allocator, "assets/floor.png", 0),
         };
 
 
@@ -37,6 +48,9 @@ pub const App = struct {
 
     pub fn deinit(self: *App) void {
         std.log.warn("shutting down app", .{});
+        self.texture1.deinit();
+        self.shader1.deinit();
+        self.mesh1.mesh.deinit();
         self.window.deinit(self.allocator);
         self.allocator.destroy(self);
     }
@@ -92,7 +106,17 @@ pub const App = struct {
             Renderer.clear();
 
 
+            self.mesh1.mesh.bind();
+            self.shader1.bind();
+            self.texture1.bind();
 
+            self.shader1.set_mat("u_view", @bitCast(self.camera1.view));
+            self.shader1.set_mat("u_projection", @bitCast(self.camera1.projection));
+            self.shader1.set_mat("u_model", @bitCast(zm.identity()));
+            self.shader1.set_int("u_texture", 0);
+
+
+            Renderer.drawMesh(&self.mesh1.mesh);
 
             self.window.swapBuffers();
         }
