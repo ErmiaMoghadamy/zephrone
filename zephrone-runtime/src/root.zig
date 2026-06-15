c94b589266ce8d649bfd61f3eca2b372bb3e8481
Ermia <ermia.codev@gmail.com>
2026-06-15 21:33:17 +0330

    REFACT: module exports

--- zephrone-runtime/src/root.zig
+++ zephrone-runtime/src/root.zig
@@ -1,15 +1,16 @@
-const std = @import("std");
-const App = @import("app.zig").App;
 
-pub fn run(io: std.Io) !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-
-    var app = try App.init(io, gpa.allocator());
-    defer app.deinit();
-
-    app.run();
-}
+pub const App = @import("./app.zig").App;
+pub const Camera = @import("./core/camera.zig").Camera;
+pub const Window = @import("./core/window.zig").Window;
+pub const Input = @import("./core/input.zig").InputManager;
+pub const Shader = @import("./graphics/shader.zig").Shader;
+pub const Mesh = @import("./graphics/mesh.zig").Mesh;
+pub const Texture = @import("./graphics/texture.zig").Texture;
+pub const Transform = @import("./graphics/transform.zig").Transform;
+pub const Renderer = @import("./graphics/renderer.zig").Renderer;
+pub const Vertex = @import("./graphics/vertex_array.zig").Vertex;
+pub const event = @import("./core/event.zig");
+pub const zmath = @import("zmath");
 
 test {
     @import("std").testing.refAllDecls(@This());
