1f1a987cccd7e9c9ee4eb0a30163d5aa1db4ad2e
ErmiaMoghadamy <ermia1303@gmail.com>
2026-06-21 15:39:56 +0330

    refact: app instance
    App is now unaware of GLState & Platform instance will handle Graphics

--- zephrone/src/app.zig
+++ zephrone/src/app.zig
@@ -1,78 +1,56 @@
-const glfw = @import("zglfw");
 const std = @import("std");
 const zm = @import("zmath");
-const event = @import("core/event.zig");
-const Window = @import("core/window.zig").Window;
-const Input = @import("core/input.zig").InputManager;
-const Time = @import("core/time.zig").Time;
+const event = @import("platform/root.zig").event;
+const Window = @import("platform/root.zig").Window;
+const Platform = @import("platform/root.zig").Platform;
 
 pub fn App(comptime GameType: type) type {
     return struct {
         game: GameType,
-        time: Time,
-        window: *Window,
+        platform: *Platform,
         allocator: std.mem.Allocator,
         io: std.Io,
 
         const Self = @This();
 
-        pub fn init(io: std.Io, allocator: std.mem.Allocator) !*Self {
-            const app = try allocator.create(Self);
-
-            const window = try Window.init(allocator, .{
+        pub fn init(io: std.Io, allocator: std.mem.Allocator) !Self {
+            const platform = try Platform.init(allocator, .{
                 .title = "Zephrone Egnine",
                 .width = null,
                 .height = null,
             });
 
-            errdefer window.deinit(allocator);
+            const init_aspect = @as(f32, @floatFromInt(platform.window.data.width)) / @as(f32, @floatFromInt(platform.window.data.height));
 
-            const init_aspect = @as(f32, @floatFromInt(window.data.width)) / @as(f32, @floatFromInt(window.data.height));
-
-            app.* = Self{
+            const app = Self{
+                .platform = platform,
                 .game = try GameType.init(io, allocator, init_aspect),
-                .time = Time.init(),
-                .window = window,
                 .allocator = allocator,
                 .io = io,
             };
-
-            window.setEventCallback(app, Self.eventCallback);
 
             return app;
         }
 
         pub fn deinit(self: *Self) void {
-            std.log.warn("shutting down app instance", .{});
+            std.log.warn("Destroying App Instance", .{});
 
             self.game.deinit();
-            self.window.deinit(self.allocator);
-            self.allocator.destroy(self);
+            self.platform.deinit(self.allocator);
         }
 
-        pub fn eventCallback(self: *Self, ev: event.ZEvent) void {
-            Input.Update(ev);
-            Input.Clear();
-
-            _ = self;
-
-            std.log.info("Event: {s}", .{@tagName(ev)});
-        }
 
         pub fn run(self: *Self) !void {
             try self.game.bootstrap();
 
-            while (!self.window.shouldCloseWindow()) {
-                Window.HandleInput();
+            while (self.platform.alive()) {
+                self.platform.pump();
 
-                self.time.update(@floatCast(Window.GetTime()));
 
-                const aspect = @as(f32, @floatFromInt(self.window.data.width)) / @as(f32, @floatFromInt(self.window.data.height));
-                try self.game.update(self.time.delta_time, aspect);
-
+                try self.game.update(self.platform.context());
                 try self.game.render();
 
-                self.window.swapBuffers();
+                self.platform.flush();
             }
         }
     };
