b855a2340166d0798575fcb6b629df01d01aaffc
ErmiaMoghadamy <ermia1303@gmail.com>
2026-06-21 15:37:45 +0330

    refact: platform module absrtactions

---
+++ zephrone/src/platform/platform.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const event = @import("./event.zig");
+const Window = @import("./window.zig").Window;
+const WindowParams = @import("./window.zig").WindowParams;
+const Time = @import("./time.zig").Time;
+const InputService = @import("../services/input.zig").InputService;
+
+pub const FrameContext = struct { dt: f32, aspect: f32 };
+
+pub const PlatformConfig = struct {
+    title: []const u8,
+    width: u32,
+    height: u32,
+};
+
+pub const Platform = struct {
+    window: *Window,
+    time: Time,
+
+    pub fn init(allocator: std.mem.Allocator, params: WindowParams) !*Platform {
+        const platform = try allocator.create(Platform);
+
+        const window = try Window.init(allocator, params);
+        errdefer window.deinit(allocator);
+
+        platform.* = .{
+            .window = window,
+            .time = Time.init(),
+        };
+
+        window.setEventCallback(platform, eventCallback);
+
+        return platform;
+    }
+
+    pub fn deinit(self: *Platform, allocator: std.mem.Allocator) void {
+        std.log.warn("Destroying Platform Instance", .{});
+
+        self.window.deinit(allocator);
+        allocator.destroy(self);
+    }
+
+    pub fn pump(self: *Platform) void {
+        Window.HandleInput();
+        self.time.update(@floatCast(Window.GetTime()));
+    }
+
+    pub fn flush(self: *Platform) void {
+        self.window.swapBuffers();
+    }
+
+    pub fn alive(self: *Platform) bool {
+        return !self.window.shouldCloseWindow();
+    }
+
+    pub fn context(self: *Platform) FrameContext {
+        const w = @as(f32, @floatFromInt(self.window.data.width));
+        const h = @as(f32, @floatFromInt(self.window.data.height));
+        return .{
+            .dt = self.time.delta_time,
+            .aspect = if (h > 0) w / h else 1.0,
+        };
+    }
+
+    pub fn eventCallback(self: *Platform, ev: event.ZEvent) void {
+        InputService.Update(ev);
+        InputService.Clear();
+
+        _ = self;
+        std.log.info("Event: {s}", .{@tagName(ev)});
+    }
+};
---
+++ zephrone/src/platform/root.zig
@@ -0,0 +1,7 @@
+pub const event = @import("event.zig");
+pub const Platform = @import("platform.zig").Platform;
+pub const PlatformConfig = @import("platform.zig").PlatformConfig;
+pub const FrameContext = @import("platform.zig").FrameContext;
+pub const Time = @import("time.zig").Time;
+pub const Window = @import("window.zig").Window;
+pub const WindowParams = @import("window.zig").WindowParams;
