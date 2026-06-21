4a13cdbe4c22c190c53728813ebf151db89bb267
ErmiaMoghadamy <ermia1303@gmail.com>
2026-06-21 15:39:03 +0330

    refact: exported modules

--- zephrone/src/root.zig
+++ zephrone/src/root.zig
@@ -1,16 +1,9 @@
-
 pub const App = @import("./app.zig").App;
-pub const Camera = @import("./core/camera.zig").Camera;
-pub const Window = @import("./core/window.zig").Window;
-pub const Input = @import("./core/input.zig").InputManager;
-pub const Shader = @import("./graphics/shader.zig").Shader;
-pub const Mesh = @import("./graphics/mesh.zig").Mesh;
-pub const Model = @import("./graphics/model.zig").Model;
-pub const Texture = @import("./graphics/texture.zig").Texture;
-pub const Transform = @import("./graphics/transform.zig").Transform;
-pub const Renderer = @import("./graphics/renderer.zig").Renderer;
-pub const Vertex = @import("./graphics/vertex_array.zig").Vertex;
-pub const event = @import("./core/event.zig");
+pub const core = @import("./core/root.zig");
+pub const graphics = @import("./graphics/root.zig");
+pub const platform = @import("./platform/root.zig");
+pub const scene = @import("./scene/root.zig");
+pub const services = @import("./services/root.zig");
 pub const zmath = @import("zmath");
 
 test {
