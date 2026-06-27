1e558a23c22b55343a4c875be942004da6e3b1e5
ErmiaMoghadamy <ermia1303@gmail.com>
2026-06-27 19:19:56 +0330

    add: adding a runtime/engine debugger

--- zephrone/src/app.zig
+++ zephrone/src/app.zig
@@ -3,13 +3,18 @@ const zm = @import("zmath");
 const event = @import("platform/root.zig").event;
 const Window = @import("platform/root.zig").Window;
 const Platform = @import("platform/root.zig").Platform;
+const Renderer = @import("graphics/renderer.zig").Renderer;
+const Debugger = @import("debug/debugger.zig").Debugger;
+const zgui = @import("zgui");
 
 pub fn App(comptime GameType: type) type {
     return struct {
-        game: GameType,
-        platform: *Platform,
         allocator: std.mem.Allocator,
         io: std.Io,
+        game: GameType,
+        platform: *Platform,
+        renderer: Renderer,
+        debugger: Debugger,
 
         const Self = @This();
 
@@ -25,8 +30,10 @@ pub fn App(comptime GameType: type) type {
             const app = Self{
                 .platform = platform,
                 .game = try GameType.init(io, allocator, init_aspect),
+                .renderer = Renderer.init(),
                 .allocator = allocator,
                 .io = io,
+                .debugger = Debugger.init(allocator, platform.window.window)
             };
 
             return app;
@@ -35,7 +42,9 @@ pub fn App(comptime GameType: type) type {
         pub fn deinit(self: *Self) void {
             std.log.warn("Destroying App Instance", .{});
 
+            self.debugger.deinit();
             self.game.deinit(self.allocator);
+            self.renderer.deinit();
             self.platform.deinit(self.allocator);
         }
 
@@ -45,8 +54,14 @@ pub fn App(comptime GameType: type) type {
             while (self.platform.alive()) {
                 self.platform.pump();
 
-                try self.game.update(self.platform.context());
-                try self.game.render();
+                const ctx = self.platform.context();
+
+                try self.game.update(ctx);
+                try self.game.render(&self.renderer);
+
+                const size = self.platform.window.getSize();
+
+                self.debugger.debugFrame(ctx, size);
 
                 self.platform.flush();
             }
--- zephrone/src/graphics/root.zig
+++ zephrone/src/graphics/root.zig
@@ -1,5 +1,6 @@
 pub const buffer = @import("buffer.zig");
 pub const Mesh = @import("mesh.zig").Mesh;
 pub const Renderer = @import("renderer.zig").Renderer;
+pub const RendererContext = @import("context.zig").RendererContext;
 pub const Shader = @import("shader.zig").Shader;
 pub const Texture = @import("texture.zig").Texture;
